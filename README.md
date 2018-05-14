7c9576eb0ffc555f8915fd788753c041cceec75b
Vasilii Komarov <komarov.vasiliy884@gmail.com>
2018-05-14 22:46:45 +0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,12 @@
+=================
+== Music store ==
+=================
+
+The course project on the discipline of "Databases"
+
+Student: Vasilii Komarov
+Number of student's record-book: 0911250
+Group: IV-44-ZF
+
+Created on 5/14/2018
+Email: komarov.vasiliy884@gmail.com
---
+++ src/main/java/com/db/courseproject/musicstore/MusicStoreApplication.java
@@ -0,0 +1,27 @@
+package com.db.courseproject.musicstore;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Application entry point.
+ * <p>
+ * Music store.
+ * The course project on the discipline of "Databases".
+ * Student: Vasilii Komarov.
+ * Group: IV-44-ZF.
+ * <p>
+ * Created on 5/14/2018.
+ *
+ * @author Vasilii Komarov
+ */
+public class MusicStoreApplication extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
