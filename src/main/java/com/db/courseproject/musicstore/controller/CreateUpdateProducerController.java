8b9695f0c8739aebac7f0940faff61ef4d748031
Vasilii Komarov <komarov.vasiliy884@gmail.com>
2018-06-18 22:32:30 +0400

    ProducerController has been implemented.

--- src/main/java/com/db/courseproject/musicstore/controller/CreateUpdateProducerController.java
+++ src/main/java/com/db/courseproject/musicstore/controller/CreateUpdateProducerController.java
@@ -1,8 +1,20 @@
 package com.db.courseproject.musicstore.controller;
 
+import com.db.courseproject.musicstore.model.FullName;
+import com.db.courseproject.musicstore.model.Producer;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import org.apache.log4j.Logger;
 
 import java.net.URL;
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 /**
@@ -12,8 +24,111 @@ import java.util.ResourceBundle;
  * @author Vasilii Komarov
  */
 public class CreateUpdateProducerController implements Initializable {
+    private static final Logger LOGGER = Logger.getLogger(CreateUpdateProducerController.class);
+
+    @FXML
+    private TextField tfId;
+    @FXML
+    private TextField tfFirstName;
+    @FXML
+    private TextField tfLastName;
+    @FXML
+    private DatePicker dpBirthDate;
+
+    @FXML
+    private Button btConfirm;
+    @FXML
+    private Button btCancel;
+
+    private ProducerController producerController;
+    private boolean isCreationOperation;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        addListenersOnTextFields();
+    }
 
+    @FXML
+    private void btConfirmClick(ActionEvent actionEvent) {
+        if (checkRequiredFields()) {
+            try {
+                Producer producer = new Producer();
+                producer.setName(getName());
+                producer.setBirthDate(getBirthDate());
+
+                if (isCreationOperation) {
+                    producerController.createProducer(producer);
+                } else {
+                    Long id = getId();
+                    producer.setId(id);
+                    producerController.updateProducer(producer, id);
+                }
+
+                btCancelClick(null);
+            } catch (NumberFormatException e) {
+                LOGGER.error(e.getMessage());
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText("Operation does not possible");
+                alert.setContentText(e.getMessage());
+                alert.showAndWait();
+            }
+        }
+    }
+
+    @FXML
+    private void btCancelClick(ActionEvent actionEvent) {
+        Stage stage = (Stage) btCancel.getScene().getWindow();
+        stage.close();
+    }
+
+    protected void setTfIdDisable(boolean param) {
+        tfId.setDisable(param);
+        isCreationOperation = param;
+    }
+
+    protected void setProducerController(ProducerController producerController) {
+        this.producerController = producerController;
+    }
+
+    private boolean checkRequiredFields() {
+        if (tfFirstName.getText().isEmpty()
+                || tfLastName.getText().isEmpty()
+                || (!isCreationOperation && tfId.getText().isEmpty())) {
+            LOGGER.info("Not all required fields are filled");
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Operation does not possible");
+            alert.setContentText("Fill all required fields");
+            alert.showAndWait();
+            return false;
+        }
+        return true;
+    }
+
+    private Long getId() {
+        return Long.parseLong(tfId.getText());
+    }
+
+    private FullName getName() {
+        return new FullName()
+                .setFirstName(tfFirstName.getText())
+                .setLastName(tfLastName.getText());
+    }
+
+    private Date getBirthDate() {
+        if (dpBirthDate.getValue() == null) {
+            return null;
+        } else {
+            return Date.from(dpBirthDate.getValue().atStartOfDay(ZoneId.systemDefault()).toInstant());
+        }
+    }
+
+    private void addListenersOnTextFields() {
+        tfId.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.matches("\\d*")) {
+                tfId.setText(newValue.replaceAll("[^\\d]", ""));
+            }
+        });
     }
 }
--- src/main/java/com/db/courseproject/musicstore/controller/ProducerController.java
+++ src/main/java/com/db/courseproject/musicstore/controller/ProducerController.java
@@ -1,10 +1,260 @@
 package com.db.courseproject.musicstore.controller;
 
+import com.db.courseproject.musicstore.dao.ProducerDAO;
+import com.db.courseproject.musicstore.exception.ForeignKeyViolationException;
+import com.db.courseproject.musicstore.model.FullName;
+import com.db.courseproject.musicstore.model.Producer;
+import com.db.courseproject.musicstore.service.ProducerService;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Date;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import static com.db.courseproject.musicstore.util.EntitiesConstants.ID;
+import static com.db.courseproject.musicstore.util.EntitiesConstants.MEMBER_BIRTH_DATE;
+import static com.db.courseproject.musicstore.util.ViewConstants.CSS_FILE;
+import static com.db.courseproject.musicstore.util.ViewConstants.MAIN_FORM_FXML;
+import static com.db.courseproject.musicstore.util.ViewConstants.MAIN_FORM_TITLE;
+import static com.db.courseproject.musicstore.util.ViewConstants.CREATE_UPDATE_PRODUCER_CONTROLLER_FXML;
+import static com.db.courseproject.musicstore.util.ViewConstants.CREATE_PRODUCER_CONTROLLER_TITLE;
+import static com.db.courseproject.musicstore.util.ViewConstants.UPDATE_PRODUCER_CONTROLLER_TITLE;
+
 /**
  * <p>
  * Created on 6/11/2018.
  *
  * @author Vasilii Komarov
  */
-public class ProducerController {
+public class ProducerController implements Initializable {
+    private static final Logger LOGGER = Logger.getLogger(ProducerController.class);
+
+    @FXML
+    private TableView<Producer> producerTable;
+    @FXML
+    private TableColumn<Producer, Long> clId;
+    @FXML
+    private TableColumn<Producer, String> clFirstName;
+    @FXML
+    private TableColumn<Producer, String> clLastName;
+    @FXML
+    private TableColumn<Producer, Date> clBirthDate;
+
+    @FXML
+    private Button btFindById;
+    @FXML
+    private Button btFindByName;
+    @FXML
+    private Button btFindAll;
+    @FXML
+    private Button btCreate;
+    @FXML
+    private Button btUpdate;
+    @FXML
+    private Button btDeleteById;
+    @FXML
+    private Button btMainMenu;
+
+    @FXML
+    private TextField tfFindById;
+    @FXML
+    private TextField tfFindByName;
+    @FXML
+    private TextField tfDeleteById;
+
+    private ObservableList<Producer> producers;
+
+    private ProducerService producerService;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        producerService = new ProducerService(new ProducerDAO());
+        setCellValueFactories();
+        producers = FXCollections.observableArrayList();
+        producerTable.setItems(producers);
+        addListenersOnTextFields();
+    }
+
+    @FXML
+    private void btFindByIdClick(ActionEvent actionEvent) {
+        this.producers.clear();
+        try {
+            Long id = Long.parseLong(tfFindById.getText());
+            Producer producer = producerService.findById(id);
+            this.producers.add(producer);
+        } catch (NumberFormatException e) {
+            LOGGER.error(e.getMessage());
+        }
+    }
+
+    @FXML
+    private void btFindByNameClick(ActionEvent actionEvent) {
+        this.producers.clear();
+        String name = tfFindByName.getText();
+        List<Producer> producers = producerService.findAllByName(name);
+        this.producers.addAll(producers);
+    }
+
+    @FXML
+    private void btFindAllClick(ActionEvent actionEvent) {
+        this.producers.clear();
+        List<Producer> producers = producerService.findAll();
+        this.producers.addAll(producers);
+    }
+
+    @FXML
+    private void btCreateClick(ActionEvent actionEvent) {
+        showCreateUpdateWindow(CREATE_UPDATE_PRODUCER_CONTROLLER_FXML,
+                CREATE_PRODUCER_CONTROLLER_TITLE, true);
+    }
+
+    @FXML
+    private void btUpdateClick(ActionEvent actionEvent) {
+        showCreateUpdateWindow(CREATE_UPDATE_PRODUCER_CONTROLLER_FXML,
+                UPDATE_PRODUCER_CONTROLLER_TITLE, false);
+    }
+
+    @FXML
+    private void btDeleteByIdClick(ActionEvent actionEvent) {
+        this.producers.clear();
+        try {
+            Long id = Long.parseLong(tfDeleteById.getText());
+            producerService.delete(id);
+        } catch (NumberFormatException e) {
+            LOGGER.error(e.getMessage());
+        } catch (ForeignKeyViolationException e) {
+            LOGGER.error(e.getMessage());
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error producer deleting");
+            alert.setHeaderText("Operation does not possible");
+            alert.setContentText(e.getMessage());
+            alert.showAndWait();
+        }
+    }
+
+    @FXML
+    private void btMainMenuClick(ActionEvent actionEvent) {
+        Stage primaryStage = new Stage();
+        URL fxmlFile = ClassLoader.getSystemClassLoader().getResource(MAIN_FORM_FXML);
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(fxmlFile);
+            Pane root = fxmlLoader.load();
+
+            Scene scene = new Scene(root);
+
+            URL cssFile = ClassLoader.getSystemClassLoader().getResource(CSS_FILE);
+            scene.getStylesheets().add((cssFile).toExternalForm());
+
+            primaryStage.setTitle(MAIN_FORM_TITLE);
+            primaryStage.setResizable(false);
+            primaryStage.setScene(scene);
+            primaryStage.initModality(Modality.WINDOW_MODAL);
+            primaryStage.show();
+
+            ((Stage) ((Node) (actionEvent.getSource())).getScene().getWindow()).close();
+        } catch (IOException e) {
+            LOGGER.error(String.format("Error loading fxml-file: %s", MAIN_FORM_FXML));
+        }
+    }
+
+    protected void createProducer(Producer producer) {
+        this.producers.clear();
+        Producer createdProducer = producerService.create(producer);
+        this.producers.add(createdProducer);
+    }
+
+    protected void updateProducer(Producer producer, Long id) {
+        this.producers.clear();
+        Producer updatedProducer = producerService.update(producer, id);
+        this.producers.add(updatedProducer);
+    }
+
+    private void showCreateUpdateWindow(String fxmlFileName, String title, boolean isCreationOperation) {
+        Stage createUpdateProducerStage = new Stage();
+        URL fxmlFile = ClassLoader.getSystemClassLoader().getResource(fxmlFileName);
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(fxmlFile);
+            Pane root = fxmlLoader.load();
+
+            CreateUpdateProducerController controller = fxmlLoader.getController();
+            controller.setProducerController(this);
+            controller.setTfIdDisable(isCreationOperation);
+
+            Scene scene = new Scene(root);
+
+            URL cssFile = ClassLoader.getSystemClassLoader().getResource(CSS_FILE);
+            scene.getStylesheets().add((cssFile).toExternalForm());
+
+            Stage mainStage = (Stage) btCreate.getScene().getWindow();
+
+            createUpdateProducerStage.setTitle(title);
+            createUpdateProducerStage.setResizable(false);
+            createUpdateProducerStage.setScene(scene);
+            createUpdateProducerStage.initModality(Modality.WINDOW_MODAL);
+            createUpdateProducerStage.initOwner(mainStage);
+            createUpdateProducerStage.show();
+        } catch (IOException e) {
+            LOGGER.error(String.format("Error loading fxml-file: %s", fxmlFileName));
+        }
+    }
+
+    private void addListenersOnTextFields() {
+        tfFindById.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.matches("\\d*")) {
+                tfFindById.setText(newValue.replaceAll("[^\\d]", ""));
+            }
+        });
+        tfDeleteById.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.matches("\\d*")) {
+                tfDeleteById.setText(newValue.replaceAll("[^\\d]", ""));
+            }
+        });
+    }
+
+    private void setCellValueFactories() {
+        clId.setCellValueFactory(new PropertyValueFactory<>(ID));
+        clFirstName.setCellValueFactory(param -> {
+            String firstName = null;
+            Producer producer = param.getValue();
+            if (producer != null) {
+                FullName fullName = producer.getName();
+                if (fullName != null) {
+                    firstName = fullName.getFirstName();
+                }
+            }
+            return new SimpleStringProperty(firstName);
+        });
+        clLastName.setCellValueFactory(param -> {
+            String lastName = null;
+            Producer producer = param.getValue();
+            if (producer != null) {
+                FullName fullName = producer.getName();
+                if (fullName != null) {
+                    lastName = fullName.getLastName();
+                }
+            }
+            return new SimpleStringProperty(lastName);
+        });
+        clBirthDate.setCellValueFactory(new PropertyValueFactory<>(MEMBER_BIRTH_DATE));
+    }
 }
